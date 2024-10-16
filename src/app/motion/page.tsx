551abdd501188a6a0373ff871cc3c895cb7c1e14
Azureit <d.f.18th@gmail.com>
2024-10-16 20:40:50 +0900

    c

--- src/app/motion/page.tsx
+++ src/app/motion/page.tsx
@@ -13,10 +13,10 @@ export default function Home() {
         >
             <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
                 <div className="shrink-0">
-                    <Image src="/azure.svg" width={48} height={48} alt="ChitChat Logo"/>
+                    <img className="size-12" src="/favicon.ico"  alt="Azure Logo"/>
                 </div>
                 <div>
-                    <div className="text-xl font-medium text-black">ChitChat</div>
+                    <div className="text-xl font-medium text-black">Azure</div>
                     <p className="text-slate-500">You have a new message!</p>
                 </div>
             </div>
