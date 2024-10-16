44edb5f0f1c722073805d427f28bbd69c246a4b7
Azureit <d.f.18th@gmail.com>
2024-10-16 20:43:52 +0900

    c

--- src/app/motion/page.tsx
+++ src/app/motion/page.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-900">
         <motion.div
             initial={{ opacity: 0 }} // 初期状態
             animate={{ opacity: 1 }} // アニメーション後の状態
