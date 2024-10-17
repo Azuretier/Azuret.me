000f55832b73f079ee02a94274eb123431ca2a42
Azureit <d.f.18th@gmail.com>
2024-10-18 02:02:11 +0900

    c

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)', // 画像のパス
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=31536000, immutable', // 1年間キャッシュ
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
