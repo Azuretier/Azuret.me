90f11c48b41d6c604f18a4f60caec0e34fd979ea
Azureit <d.f.18th@gmail.com>
2024-10-20 06:17:24 +0900

    change

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -5,11 +5,11 @@
 import {useEffect} from 'react'
 import {animate} from 'framer-motion/dom'
 
-const fadeUpElement = document.getElementById("fadeUpElement")
-
 const Gatiiku = () => {
 
     useEffect(() => {
+        const fadeUpElement = document.getElementById("fadeUpElement")
+        
         // ページが読み込まれた時に isLoaded を true に設定
         if (fadeUpElement !== null) {
             animate(fadeUpElement, { y: [10, 0], opacity: [0, 1] }, { type: "spring" })
