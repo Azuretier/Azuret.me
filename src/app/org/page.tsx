295cc2951821e08d8514b4fba77a4228fb242536
Azureit <d.f.18th@gmail.com>
2024-10-20 00:36:01 +0900

    c

--- src/app/org/page.tsx
+++ src/app/org/page.tsx
@@ -2,13 +2,20 @@
 
 //import Center from "@/components/org/c"
 //import {FadeUpDiv, FadeUpStagger} from "@/components/animation"
-import {motion} from "framer-motion"
+import {useState, useEffect} from 'react'
+import {animate} from 'framer-motion/dom'
 
 const Gatiiku = () => {
+    const [isLoaded, setIsLoaded] = useState<boolean>(false); // 初期状態は false
+
+    useEffect(() => {
+        // ページが読み込まれた時に isLoaded を true に設定
+        animate("div", { x: [0, 100] }, { type: "spring" })
+    }, []); // 空の依存配列で初回レンダリング時にのみ実行
 
     return (
         <main className="flex items-center justify-center h-screen">    
-            <motion.div animate={{ x: 100 }} />
+            <div>gatiiku</div>
         </main>
     )
 }
